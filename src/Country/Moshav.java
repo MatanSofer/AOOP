0766a4299541cad9fef7e7f130be732a6925c989
Maor <maorar1@ac.sce.ac.il>
2021-04-22 23:45:59 +0300

    Ramzor updates

--- src/Country/Moshav.java
+++ src/Country/Moshav.java
@@ -19,15 +19,19 @@ public class Moshav extends Settlement
         double c = getRamzorRating();
 		double p = contagiousPercent();
 		double result = 0.3+ 3*(Math.pow((Math.pow(1.2,c))*(p-0.35),5));
+		
 		this.setRamzorRating(result);
-		if (result <= 0.4)
-			return RamzorColor.Green;
-		else if (result <= 0.6)
-			return RamzorColor.Yellow;
-		else if (result <= 0.8)
-			return RamzorColor.Orange;
-		else
-			return RamzorColor.Red;		
+		
+//		if (result <= 0.4)
+//			return RamzorColor.Green;
+//		else if (result <= 0.6)
+//			return RamzorColor.Yellow;
+//		else if (result <= 0.8)
+//			return RamzorColor.Orange;
+//		else
+//			return RamzorColor.Red;	
+		
+		return RamzorColor.gradeToColor(result);
 	}
 
 }
